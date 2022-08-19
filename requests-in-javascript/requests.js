27adef08474fc2cc7ffd36b2a6b7b3d90ed63911
Denasia <dfurniss@ymail.com>
2022-08-19 15:29:17 -0400

    configured for deployment

---
+++ requests-in-javascript/requests.js
@@ -0,0 +1,44 @@
+const axios = require("axios");
+
+// const url = "http://localhost:5000/constellations";
+
+// function getConstNames() {
+//     axios
+//     .get(url)
+//     .then(({data})=>{
+//         let result = data.map(star => star.name).sort((starA, starB)=> starA < starB ? 1:-1)
+//         console.log(result)
+//         // console.log(result)
+//         return result
+//     })
+//     .catch(({message})=>console.log(message))
+// }
+
+// function addNewConst(obj){
+//     axios
+//     .post(url, obj)
+// }
+// getConstNames()
+
+
+const BASE_URL = "http://localhost:5000";
+
+Promise.all([
+  axios.get(`${BASE_URL}/constellations/UEUrlfX`),
+  axios.get(`${BASE_URL}/constellations/zb8QvVt`),
+  axios.get(`${BASE_URL}/constellations/32TN5F8`),
+]).then(results => {
+    const newRes = results.map(result=>result.data)
+    console.log(newRes)
+});
+
+// axios
+//   .get(url)
+//   .then((response) => {
+//     // console.log(response.status);
+//     // console.log(response.statusText);
+//     console.log(response.data.filter(star => star.starsWithPlanets < 10));
+//   })
+//   .catch((error) => {
+//     console.log(error.message);
+//   });
